eb613228411745d7e03095c9e16b11828fc4ef75
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2024-02-04 17:20:38 +0100

    Section Three

--- API/Program.cs
+++ API/Program.cs
@@ -9,13 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
